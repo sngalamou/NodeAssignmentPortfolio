877d2a3812c44d3bcacb39ffe3cc5737ca1cd87a
Simeon Ngalamou <simeonngalamou@lewisu.edu>
2022-03-31 12:18:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
   
-
+ 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
